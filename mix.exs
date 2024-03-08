081bce170b9a2532c3ebf6983d6a75ca59d63afa
Tom Monks <monksy@gmail.com>
2024-03-08 10:25:56 -0500

    Add Req

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule ChatBots.MixProject do
       {:ecto_sql, "~> 3.6"},
       {:ecto_sqlite3, "~> 0.9.1"},
       {:phoenix_ecto, "~> 4.4"},
-      {:dotenvy, "~> 0.7.0"}
+      {:dotenvy, "~> 0.7.0"},
+      {:req, "~> 0.4.0"}
     ]
   end
 
