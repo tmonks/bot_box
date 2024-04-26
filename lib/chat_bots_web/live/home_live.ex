24319443ce924c73a0d7f50fd85520e1ce2b7b2e
Tom Monks <monksy@gmail.com>
2024-04-26 16:45:19 -0400

    HomeLive lists chats with bot names

--- lib/chat_bots_web/live/home_live.ex
+++ lib/chat_bots_web/live/home_live.ex
@@ -1,21 +1,23 @@
 defmodule ChatBotsWeb.HomeLive do
   use ChatBotsWeb, :live_view
+  alias ChatBots.Chats
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    chats = Chats.list_chats()
+
+    {:ok, assign(socket, chats: chats)}
   end
 
   @impl true
   def render(assigns) do
     ~H"""
     <div>
-      <h1>Chat List</h1>
-      <ul>
-        <li>Chat 1</li>
-        <li>Chat 2</li>
-        <li>Chat 3</li>
-      </ul>
+      <%= for chat <- @chats do %>
+        <div id={"chat-#{chat.id}"}>
+          <%= chat.bot.name %>
+        </div>
+      <% end %>
     </div>
     """
   end
--- test/chat_bots_web/live/home_live_test.exs
+++ test/chat_bots_web/live/home_live_test.exs
@@ -1,11 +1,24 @@
 defmodule ChatBotsWeb.HomeLiveTest do
-  use ChatBotsWeb.ConnCase
+  use ChatBotsWeb.ConnCase, async: false
   import Phoenix.LiveViewTest
+  import ChatBots.Factory
+
+  setup :login_user
 
   test "lists existing chats", %{conn: conn} do
     chat = insert(:chat)
     {:ok, view, _html} = live(conn, "/")
 
     assert has_element?(view, "#chat-#{chat.id}")
+  end
+
+  test "shows the bot's name for each chat", %{conn: conn} do
+    bot = insert(:bot, name: "Bob")
+    chat = insert(:chat, bot: bot)
+    {:ok, view, _html} = live(conn, "/")
+
+    open_browser(view)
+
+    assert has_element?(view, "#chat-#{chat.id}", "Bob")
   end
 end
