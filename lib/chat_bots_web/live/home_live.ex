20408570b67880678e42bd17200e502fd10f73e1
Tom Monks <monksy@gmail.com>
2024-04-26 17:39:40 -0400

    Show relative time of last message

--- lib/chat_bots_web/live/home_live.ex
+++ lib/chat_bots_web/live/home_live.ex
@@ -1,6 +1,7 @@
 defmodule ChatBotsWeb.HomeLive do
   use ChatBotsWeb, :live_view
   alias ChatBots.Chats
+  alias Timex.Format.DateTime.Formatters.Relative
 
   @impl true
   def mount(_params, _session, socket) do
@@ -13,9 +14,19 @@ defmodule ChatBotsWeb.HomeLive do
   def render(assigns) do
     ~H"""
     <div>
+      <div class="mt-0 mb-2 text-4xl font-medium leading-tight text-primary">
+        Bot Box
+      </div>
       <%= for chat <- @chats do %>
-        <div id={"chat-#{chat.id}"}>
-          <%= chat.bot.name %>
+        <div id={"chat-#{chat.id}"} class="flex flex-row items-center">
+          <div>😃</div>
+          <div class="flex flex-col">
+            <div><%= chat.bot.name %></div>
+            <div>Message text here</div>
+          </div>
+          <div data-role="time">
+            <%= Relative.format!(chat.latest_message.inserted_at, "{relative}") %>
+          </div>
         </div>
       <% end %>
     </div>
--- test/chat_bots_web/live/home_live_test.exs
+++ test/chat_bots_web/live/home_live_test.exs
@@ -17,8 +17,22 @@ defmodule ChatBotsWeb.HomeLiveTest do
     chat = insert(:chat, bot: bot)
     {:ok, view, _html} = live(conn, "/")
 
-    open_browser(view)
-
     assert has_element?(view, "#chat-#{chat.id}", "Bob")
+  end
+
+  test "shows the relative time of the last message on each chat", %{conn: conn} do
+    chat = insert(:chat)
+    message = insert(:message, chat: chat, inserted_at: Timex.now() |> Timex.shift(minutes: -5))
+
+    {:ok, view, _html} = live(conn, "/")
+
+    assert has_element?(
+             view,
+             "#chat-#{chat.id} div[data-role=time]",
+             "5 minutes ago"
+           )
+  end
+
+  test "shows an excerpt of the last message on each chat", %{conn: conn} do
   end
 end
