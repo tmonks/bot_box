700f17c3fdda3270c54102455083dc310e47a7cb
Tom Monks <monksy@gmail.com>
2024-05-02 09:56:03 -0400

    First pass at HomeLive design improvement

--- lib/chat_bots_web/live/home_live.ex
+++ lib/chat_bots_web/live/home_live.ex
@@ -29,29 +29,38 @@ defmodule ChatBotsWeb.HomeLive do
         Bot Box
       </div>
       <form id="bot-select-form" phx-submit="new_chat">
-        <select
-          id="bot-select"
-          name="bot_id"
-          class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline mb-2"
-        >
-          <%= options_for_select(bot_options(@bots), []) %>
-        </select>
-        <button class="bg-primary" type="submit">New Chat</button>
+        <div class="flex gap-4 items-center mb-4">
+          <select
+            id="bot-select"
+            name="bot_id"
+            class="flex-auto block appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+          >
+            <%= options_for_select(bot_options(@bots), []) %>
+          </select>
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            New Chat
+          </button>
+        </div>
       </form>
-      <%= for chat <- @chats do %>
-        <.link navigate={~p"/chat/#{chat.id}"} id={"chat-#{chat.id}"}>
-          <div class="flex flex-row items-center">
-            <div>😃</div>
-            <div class="flex flex-col">
-              <div><%= chat.bot.name %></div>
-              <div data-role="content"><%= chat.latest_message.content %></div>
+      <div class="flex flex-col gap-6">
+        <%= for chat <- @chats do %>
+          <.link navigate={~p"/chat/#{chat.id}"} id={"chat-#{chat.id}"}>
+            <div class="flex flex-row items-center gap-4">
+              <div class="w-32">😃</div>
+              <div class="flex flex-col">
+                <div class="text-lg font-bold"><%= chat.bot.name %></div>
+                <div data-role="content"><%= chat.latest_message.content %></div>
+              </div>
+              <div class="w-80" data-role="time">
+                <%= Relative.format!(chat.latest_message.inserted_at, "{relative}") %>
+              </div>
             </div>
-            <div data-role="time">
-              <%= Relative.format!(chat.latest_message.inserted_at, "{relative}") %>
-            </div>
-          </div>
-        </.link>
-      <% end %>
+          </.link>
+        <% end %>
+      </div>
     </div>
     """
   end
